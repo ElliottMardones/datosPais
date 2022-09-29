2869a523cf1579f147929fe6919e115abc6fb7c0
Elliott Mardones <elliottmardones@gmail.com>
2022-09-29 17:06:03 -0300

    u
    u

--- R/datos.R
+++ R/datos.R
@@ -1,18 +1,45 @@
-# Hello, world!
-#
-# This is an example function named 'hello' 
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
+library(datosPxP)
+datos <- datosPxP::datos
+usethis::use_data(datos)
 
-hello <- function() {
-  print("Hello, world!")
+#Normalizar los datos
+
+# 1) Normalizacion de los datos dividiendo el valor mas grande (por pais)
+
+#' maxByYear
+#' @export
+maxByYear <- function(Data){
+  experts <- dim(Data)[3]
+  for( expert in seq_len(experts)){
+    maxValuebyYear <- (max(Data[,,expert]))
+    Data[,,expert] <- Data[,,expert]/maxValuebyYear
+    diag(Data[,,expert]) <- 1
+  }
+  return(Data)
 }
+
+# 2)
+
+#' max_ByCountry
+#' @export
+max_ByCountry <- function(Data){
+  experts <- dim(Data)[3]
+  countrys <- dim(Data)[2]
+  for( expert in seq_len(experts)){
+    for(country in seq_len(countrys)){
+      Data[country, ,expert] <- (Data[country, ,expert])/(sum(Data[country, ,expert]))
+    }
+    diag(Data[,,expert]) <- 1
+  }
+  return(Data)
+}
+
+byYear <- maxByYear(datos)
+normalized_by_year <- byYear
+
+byCountry <- max_ByCountry(datos)
+normalized_by_country <- byCountry
+
+
+usethis::use_data(normalized_by_year, normalized_by_year)
+usethis::use_data(normalized_by_country, normalized_by_country)
