3199285d2983b7ac16201956dacc090095a3aeb2
Elliott Mardones <elliottmardones@gmail.com>
2022-12-09 22:42:37 -0300

    u
    u

--- R/graficador.R
+++ R/graficador.R
@@ -16,11 +16,12 @@ myPlot <- function(Data,type = c("Pr", "Bw"), vertex_size = 0.5, arrow_size = 0.
   myGraph <- graph.adjacency(AAadj, weighted = T, mode = "directed")
   myGcent <- graph.adjacency(AAcent, weighted = T, mode = "directed")
   color <- c()
-  AFRICA <-  "#48C9B0"
-  AMERICA <- "#5DADE2"
-  ASIA <- "#EC7063"
-  EUROPA <- "#F4D03F"
-  OCEANIA <- "#E67E22"
+  AFRICA <-  "#B530BB" #moraddo
+  AMERICA <- "#2980B9" #azul
+  ASIA <- "#D35400" # casi rojo
+  EUROPA <- "#F1C40F" # amarillo
+  OCEANIA <- "#95A5A6"# gris
+  South_America <- "#2ECC71" #verde
   for(i in seq_len(length(myGraph))){
     #browser()
     for(j in seq_len(length(paises$ISO.3166))){
@@ -40,18 +41,31 @@ myPlot <- function(Data,type = c("Pr", "Bw"), vertex_size = 0.5, arrow_size = 0.
         else if(paises$Continente[j] == 4){ # OCEANIA --- ORANGE
           color <- c(color,OCEANIA)
         }
+        else if(paises$Continente[j] == 5){ # South America
+          color <- c(color, South_America)
+        }
       }
     }
   }
   valores<-sort(unique(as.vector(AAadj)))
   #
-  valorespaleta <- 1- (1/exp(valores*2))
+  valorespaleta <- 1- (1/exp(valores*6.2))
   #valorespaleta[1]<-0
   #
   paleta<- grey(rep(0,length(valores)),valorespaleta) #^color
   #paleta <- paleta[length(paleta):1]
   vertex.size <- 0
   vertex.label.cex <- 0
+
+  df_betweenness_tb <- data.frame()
+  df_pagerank_ <- data.frame()
+  # calculo de la centralidad betweenness y pagerank
+
+  df_betweenness_tb <- as.data.frame(betweenness(myGcent,directed = T))
+  colnames(df_betweenness_tb) <- "Betweenness"
+  # pagerank
+  df_pagerank_ <- as.data.frame(page.rank(myGcent,directed = T)$vector)
+  colnames(df_pagerank_) <- "PageRank"
   # que tipo de vertex.size usar, si page.rank o betweeneess
   if(type == "Bw"){
     bet <- (betweenness(myGcent,directed = T))
@@ -61,8 +75,8 @@ myPlot <- function(Data,type = c("Pr", "Bw"), vertex_size = 0.5, arrow_size = 0.
     vertex.label.cex <- 1*sqrt((bet))+0.3
   }
   else if(type == "Pr"){
-    vertex.size <- 22*(page.rank(myGcent,directed = T)$vector)^vertex_size
-    vertex.label.cex <-  1*sqrt((page.rank(myGcent,directed = T)$vector))+vertex_size
+    vertex.size <- 27*(page.rank(myGcent,directed = T)$vector)^vertex_size
+    vertex.label.cex <-  1*sqrt((page.rank(myGcent,directed = T)$vector))+0.3
   }
   #return(igraph::cliques(myGraph, min = 3, max = 6))
   par(mar=c(0.5,0.5, 0.5, 0.5))
@@ -83,9 +97,11 @@ myPlot <- function(Data,type = c("Pr", "Bw"), vertex_size = 0.5, arrow_size = 0.
   )
   legend( x = "topleft",
           title = "Continents",
-          legend = c("Africa", "America", "Asia", "Europe", "Oceania" ),
-          fill = c(AFRICA, AMERICA, ASIA, EUROPA, OCEANIA ),
+          legend = c("NA", "AS", "EU", "OC", "SA", "AF" ),
+          fill = c( AMERICA, ASIA, EUROPA, OCEANIA , South_America, AFRICA),
           border = "white"
   )
-  return(igraph::cliques(myGraph, min = 3, max = 6))
+  centralidades <- cbind( round(df_pagerank_ , 3), df_betweenness_tb)
+  centralidades <- centralidades[order(centralidades$PageRank, decreasing = T),]
+  return(centralidades)
 }
